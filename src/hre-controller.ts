24039329e063cbe3db44243a95e89d26ae7a7d52
jin <sifnoc09@gmail.com>
2022-02-22 12:02:26 +0900

    feat: create methods for mining mode

--- src/hre-controller.ts
+++ src/hre-controller.ts
@@ -1,14 +1,24 @@
 // import hre from 'hardhat'
 import { HardhatRuntimeEnvironment as HRE } from 'hardhat/types'
 
+enum MiningMode {
+  STOPPED,
+  MANUAL,
+  INSTANT,
+  INTERVAL,
+}
+
 export class HreController {
   hre: HRE
 
   currentBlockNum: number
 
+  currentMiningMode: MiningMode
+
   constructor(hre: HRE) {
     this.hre = hre
     this.currentBlockNum = 0
+    this.currentMiningMode = MiningMode.STOPPED
   }
 
   static async getBlockNumber(hre: HRE) {
@@ -21,29 +31,64 @@ export class HreController {
   }
 
   async resetFork(blockNumber: number) {
-    await this.hre.network.provider.request({
-      method: "hardhat_reset",
-      params: [
-        {
-          forking: {
-            jsonRpcUrl: process.env.JSONRPC_URL ?? 'https://localhost:8545',
-            blockNumber,
+    try {
+      await this.hre.network.provider.request({
+        method: "hardhat_reset",
+        params: [
+          {
+            forking: {
+              jsonRpcUrl: process.env.JSONRPC_URL ?? 'https://localhost:8545',
+              blockNumber,
+            },
           },
-        },
-      ],
-    })
-    await this.updateBlockNumber()
+        ],
+      })
+      await this.updateBlockNumber()
+    } catch (error) {
+      throw Error(`Failed reset fork at ${blockNumber} - ${error}`)
+    }
+    this.currentMiningMode = MiningMode.STOPPED
   }
 
+  // Manual Mode
   async nextBlock() {
-    await this.hre.network.provider.send("evm_mine");
+    try {
+      await this.hre.network.provider.send("evm_mine");
+      this.currentMiningMode = MiningMode.MANUAL
+    } catch (error) {
+      this.currentMiningMode = MiningMode.STOPPED
+      throw Error(`Failed move next block - ${error}`)
+    }
   }
 
+  // Instant Mode
+  async startInstantMine() {
+    try { 
+      await this.hre.network.provider.send("evm_setIntervalMining", [])
+      this.currentMiningMode = MiningMode.INSTANT
+    } catch (error) {
+      this.currentMiningMode = MiningMode.STOPPED
+      throw Error(`Failed set instant mining mode - ${error}`)
+    }
+  }
+
+  // Interval 
   async startAutoMine(interval: number) {
-    await this.hre.network.provider.send("evm_setIntervalMining", [interval])
+    try { 
+      await this.hre.network.provider.send("evm_setIntervalMining", [interval])
+      this.currentMiningMode = MiningMode.INTERVAL
+    } catch (error) {
+      this.currentMiningMode = MiningMode.STOPPED
+      throw Error(`Failed to start Inerval mining - ${error}`)
+    }
   }
 
   async stopAutoMine() {
-    await this.hre.network.provider.send("evm_setIntervalMining", [0])
+    try { 
+      await this.hre.network.provider.send("evm_setIntervalMining", [0])
+      this.currentMiningMode = MiningMode.STOPPED
+    } catch (error) {
+      throw Error(`Failed move next block - ${error}`)
+    }
   }
 }
